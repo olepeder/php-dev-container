e467ad8f97bd7bcd1012e252be5b9f4385006917
Ole Peder Granaasen <ole.peder.granaasen@politiet.no>
2023-04-09 17:59:53 +0200

    initial PHP in container

---
+++ README.md
@@ -0,0 +1,12 @@
+---
+title: PHP development environment in Docker
+keywords: PHP development docker container
+---
+
+## How to start
+
+Run the following command in your terminal
+
+> docker-compsoe up -d --build
+
+Open browser and connect to [http://127.0.0.1](http://127.0.0.1)
---
+++ app/public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP development with Docker container</title>
+</head>
+
+<body>
+  <h1>PHP development with Docker container</h1>
+  <p>See <a href="php-info.php">phpinfo()</a></p>
+  <p>See <a href="mysql.php">MySQL info</a></p>
+</body>
+
+</html>
